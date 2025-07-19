3e9183192ec8a84d4688ef7c347def312c34b515
HamidEidy <hamidreza.eidy1999@gmail.com>
2025-07-19 19:26:15 +0330

    create 'all post' page

--- blogs/urls.py
+++ blogs/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogs.urls'))
+    path('', views.index, name='starting-page'),
+    path('posts', views.posts, name='posts-page'),
+    path('posts/<slug:slug>', views.single_post, name="post-detail-page")
 ]
